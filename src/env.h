f28e03b9cb91204ac88160b3bdeab45d29dbe4b8
Ada <adaau0209@gmail.com>
2021-03-17 00:52:03 +0800

    added player static pannel

--- src/env.h
+++ src/env.h
@@ -1,9 +1,24 @@
 #ifndef ENV_H
 #define ENV_H
+#include <string>
 
 const int map_height = 30;
 const int map_width = 60;
 const int map_maxTunnels = 1200;
 const int maxLength = 10;
+const int max_debuff = 12; // +4 is the min map size
+const std::string debuffs[max_debuff] = {
+    "Burning",
+    "Freezing",
+    "Blind",
+    "Poison",
+    "Hunger",
+    "Bleeding",
+    "Fear",
+    "Nervous",
+    "Wet",
+    "Cursed",
+    "Paralyzed",
+    "Hallucination"};
 
 #endif
