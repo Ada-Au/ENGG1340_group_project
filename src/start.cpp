ddb777a00ae7ee56484dfa34aa3bbd5f9a5b1e40
LAPTOP-N4DU5N1F\Humen Chau <humenchau@gmail.com>
2021-03-16 13:31:50 +0800

    no message

---
+++ src/start.cpp
@@ -0,0 +1,9 @@
+# include <mainChar.h>
+
+using namespace std;
+
+int main(){
+
+    cout << "YOU DIED. Welome to the Underworld!\n" << "Do you remember your name?\n" <<"Input your name: ";
+    
+}
