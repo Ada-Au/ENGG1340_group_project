4d20e9887295f1174b7a0f4e03132c295a1ee9e9
LAPTOP-N4DU5N1F\Humen Chau <humenchau@gmail.com>
2021-03-17 00:51:23 +0800

    no message

---
+++ src/player.h
@@ -0,0 +1,40 @@
+#ifndef Player
+#define Player
+
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+class Player
+{   
+    public:
+    Player();
+    string name;
+
+    string roleList[12] = {"Archeologist","Barbarian","Caveman/Cavewoman", "Healer", "Knight", "Monk", "Priest/Priestess", "Rogue", "Ranger", "Samurai", "Tourist", "Wizard" };
+    string raceList[4] = {"humen", "drawf", "elf" , "orc"};
+    string genderList[2] = {"male", "female"};
+    
+    string role;
+    string race;
+    string gender;
+
+    void randomRole(string &role, string roleList[])
+    {
+        role = roleList[rand()%sizeof(roleList)];
+    }
+
+    void randomRace(string &race, string raceList[])
+    {
+        race = raceList[rand()%sizeof(raceList)];
+    }
+
+    void randomGender(string &gender, string genderList[])
+    {
+        gender = genderList[rand()%sizeof(genderList)];
+    }
+
+};
+
+#endif
--- src/start.cpp
+++ src/start.cpp
@@ -1,9 +1,19 @@
-# include <mainChar.h>
+#include "player.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
-int main(){
 
-    cout << "YOU DIED. Welome to the Underworld!\n" << "Do you remember your name?\n" <<"Input your name: ";
+void main()
+{
+    Player player;
     
+    cout << "YOU DIED. Welome to the Underworld!\n" << "Do you remember your name?\n" <<"Input your name: ";
+    cin << player.name;
+    cout << "Here is your indentity information\n";
+    cout << "Name: " << player.name << "\n"
+         << "Role: " << player.randomRole() << "\n"
+         << "Race: " << player.randomRace() << "\n"
+         << "Gender: "<< player.randomGender() << "\n"
 }
