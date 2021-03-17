7d3dc35bb9fb8f0c2e58802d67b808f5976c41bb
LAPTOP-N4DU5N1F\Humen Chau <humenchau@gmail.com>
2021-03-18 03:47:11 +0800

    ~

--- src/player.h
+++ src/player.h
@@ -13,17 +13,16 @@ const int max_gender = 2;
 
 class Player
 {
-public:
-    Player();
-    string name;
-
-    string roleList[max_role] = {"Archeologist", "Barbarian", "Caveman/Cavewoman", "Healer", "Knight", "Monk", "Priest/Priestess", "Rogue", "Ranger", "Samurai", "Tourist", "Wizard"};
-    string raceList[max_race] = {"human", "drawf", "elf", "orc"};
-    string genderList[max_gender] = {"male", "female"};
-
-    string role;
-    string gender;
-    string race = "human";
-
+    public:
+        string name;
+        string role;
+        string gender;
+        string race = "human";
+        string roleList[max_role] = {"Archeologist", "Barbarian", "Caveman/Cavewoman", "Healer", "Knight", "Monk", "Priest/Priestess", "Rogue", "Ranger", "Samurai", "Tourist", "Wizard"};
+        string raceList[max_race] = {"human", "drawf", "elf", "orc"};
+        string genderList[max_gender] = {"male", "female"};
+        Player();
+        void setData(char flag, int max, string &role, string &race, string &gender);
+        void PrintInform();
 };
 #endif
