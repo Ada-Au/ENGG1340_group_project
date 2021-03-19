04048d10bb3c89fb31aef23300ff1a674eb33f32
LAPTOP-N4DU5N1F\Humen Chau <humenchau@gmail.com>
2021-03-19 14:06:55 +0800

    press 1,2,3 updated

--- src/homepage.cpp
+++ src/homepage.cpp
@@ -22,51 +22,90 @@ int main()
     bool selecting = true;
     bool updated = false;
     string log = "";
+
     //To-do: Press any key to continue
     //       add reminder: plz play this game with full screen
     for(int i = 0; i < 10; i++)
         cout << homepage.homepage[i] << "\n";
 
-    cout << "_START_\n" << "_LOAD_\n" << "_HELP_\n\n";
+    // Press UP DOWN to choose
+    // cout << "_START_\n" << "_LOAD_\n" << "_QUIT_\n\n";
+
+    // char c;
+    // while(selecting)
+    // {
+    //     switch(c = getch())
+    //     {   
+    //         case KEY_UP:
+    //             if (selected > 0){
+    //                 selected--;
+    //                 updated = true;
+    //             }
+    //             break;
+    //         case KEY_DOWN:
+    //             if (selected < choices - 1){
+    //                 selected++;
+    //                 updated = true;
+    //             }
+    //             break;
+    //         case KEY_ENTER:
+    //             selecting = false;
+    //             break;
+            
+    //         default:
+    //             break;
+    //     }
+    //     if (updated){
+    //         if (selected <= 0)
+    //             log = "Selected START";
+
+    //         else if (selected == 1)
+    //             log = "Selected LOAD";
+
+    //         else if (selected >= 2)
+    //             log = "Selected HELP";
+            
+    //         cout << log << "\t<Press Enter to Continue...>\n";
+    //         updated = false;
+    //     }
+    // }
+
+    //Press 1, 2, 3 to continue
+    cout << "[1]  START\t" << "[2]  LOAD\t" << "[3]  QUIT\n";
 
     char c;
+    while (selecting){
 
-    //Press UP DOWN to choose
-    while(selecting)
-    {
-        switch(c = getch())
+        switch (c = getch())
         {   
-            case KEY_UP:
-                if (selected > 0){
-                    selected--;
-                    updated = true;
-                }
-                break;
-            case KEY_DOWN:
-                if (selected < choices - 1){
-                    selected++;
-                    updated = true;
-                }
-                break;
-            case KEY_ENTER:
-                selecting = false;
-                break;
-            
             default:
                 break;
-        }
-        if (updated){
-            if (selected <= 0)
+            case '1':
                 log = "Selected START";
-
-            else if (selected == 1)
+                updated = true;
+                break;
+            case '2':
                 log = "Selected LOAD";
-
-            else if (selected >= 2)
-                log = "Selected HELP";
-            
+                updated = true;
+                break;      
+            case '3':
+                log = "Selected QUIT";
+                updated = true;
+                break; 
+            case KEY_ENTER:
+                selecting = false;
+                break;                         
+        }
+        
+        if (updated){
             cout << log << "\t<Press Enter to Continue...>\n";
-            updated = false;
+            c = getch();
+            if (c == KEY_ENTER){
+                selecting = false;
+                break;                
+            }
+            else
+                updated = false;             
         }
     }
     return 0;
