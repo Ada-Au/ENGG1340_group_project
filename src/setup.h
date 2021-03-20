4595593ea6572065382c43fab06b7a2d7d00ff22
Ada <adaau0209@gmail.com>
2021-03-20 14:35:12 +0800

    updated gitignore

--- src/setup.h
+++ src/setup.h
@@ -11,11 +11,4 @@ using namespace std;
 
 void setupScreen(Player);
 
-// void setData(char, int, Player);
-// void PrintInform(Player);
-// void PrintRole(string);
-// void PrintRace(Player);
-// void PrintGender(Player);
-// void PrintChoice();
-
 #endif
