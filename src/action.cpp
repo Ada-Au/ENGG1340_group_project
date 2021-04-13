eda11dc90246a8509bf9243e6e592085c92e0465
LAPTOP-N4DU5N1F\Humen Chau <humenchau@gmail.com>
2021-04-14 03:12:42 +0800

    updating endings

--- src/action.cpp
+++ src/action.cpp
@@ -2,6 +2,8 @@
 #include "backpack.h"
 #include <iostream>
 #include <thread>
+#include <iomanip>
+#include <stdlib.h>
 #if defined _WIN32 || defined _WIN64
 #include <conio.h>
 #define ISWINDOW true
@@ -12,6 +14,76 @@
 
 using namespace std;
 
+
+void printBoatsInEnding() {
+    string boats[11] = {"1 - trash", "0 G",
+                        "2 - wooden boat", "(Sold out)",
+                        "3 - iron boat", "(Sold out)",
+                        "4 - golden boat", "10,000 G",
+                        "5 - EPIC DIAMOND BOAT", "999,999,999 G",
+                        ""};
+    for (int i = 0; i < 10; i++) {
+        if (i == 0)
+            cout << "\nBoats" << setfill(' ') << setw(36) << "Price" << endl;
+        if (i % 2 == 0)
+            cout << boats[i] << setfill(' ') << setw(40 - boats[i].length());
+        else
+            cout << boats[i] << endl;
+    }
+}
+void tryAgainInEnding(string choice){       //To-do: adding condition -> player's property
+    printBoatsInEnding();
+    if (ISWINDOW)
+        cin.ignore();
+    getline(cin, choice);
+    if (choice[1] == '\0' && choice[0] == '1') {
+        renderNpc("charon", "You're broke before you came?");
+        renderNpc("charon", "What did you do in hell???");
+        renderNpc("charon", "Are you a brainless slime,");
+        renderNpc("charon", "that can only jump around??");
+        renderNpc("charon", "Go get a job and make money!!");
+        cout << "Bad Ending: Working in Hell.\n";
+    } else if (choice[1] == '\0' && (choice[0] == '2' || choice[0] == '3') ) {
+        renderNpc("charon", "Can't you see it is SOLD OUT??");
+        cout << "Enter again: ";
+        // getline(cin, choice);
+        tryAgainInEnding(choice);
+    } else if (choice[1] == '\0' && choice[0] == '4') {
+        renderNpc("charon", "Nice, you got enough money this time.");
+        cout << "golden boat x 1 GET!/n";
+        renderNpc("charon", "Here is your boat.");
+        renderNpc("charon", "Sail it to the end of the river.");
+        renderNpc("charon", "Enjoy your afterlife :)");
+        cout << "Normal Ending: Having an Afterlife.\n";
+    } else if (choice[1] == '\0' && choice[0] == '5') {
+        renderNpc("charon", "You work real hard.");
+        renderNpc("charon", "Proud of you for buying it. :D");
+        renderNpc("charon", "Here is your boat.");
+        renderNpc("charon", "Sail it to the Heaven, boy.");
+        cout << "Good Ending: Going to the Heaven.\n";
+    } else {
+        renderNpc("charon", "No such choice.");
+        cout << "Enter again: ";
+        // getline(cin, choice);
+        tryAgainInEnding(choice);
+    }
+        
+
+}
+void ending() {
+    string choice = " ";
+    cout << "At the end, you defeated Hades, the Lord of Hell, through out your endless, extraordinary journey here.\n";
+    cout << "You feel drained out after such long battles, playing around the cold and dark, danger and monsters\n";
+    cout << "Closing your eyes, you were just desperate for a moment of rest...\n";
+    cout << "A whistle passes into your ears, a familiar voice...\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    cout << "\"Oh, you come back to buy boats?\"\n";
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    renderNpc("charon", "Many people come and buy mt boats.");
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    renderNpc("charon", "Which one do you want?");
+    tryAgainInEnding(choice);
+}
 
 void action(Screen scr, Map map, Player player, Item item[], bool &isReplay) {
     char key = ' ';
@@ -64,12 +136,13 @@ void action(Screen scr, Map map, Player player, Item item[], bool &isReplay) {
 
         if (map.layout[player.y][player.x] == 'S') {
             player.gameLevel++;
-            bool isEnd = false;
+            bool isEnd = true;
             if (player.gameLevel % 5 == 0) {
                 cout << player.gameLevel / 5;
-                bossScreen(player, item, player.gameLevel / 5, isEnd);
+                // bossScreen(player, item, player.gameLevel / 5, isEnd);
                 player.gameLevel++;
                 if (isEnd) {
+                    ending();
                     cout << "You win the game! Wanna play again??\ny - Yes, what a fun game!\tn - No, I have better things to do with my life." << endl;
                     tryAgain("Please input again:\ny - Yes, what a fun game!\tn - No, I have better things to do with my life.", isReplay);
                     break;
--- src/player.cpp
+++ src/player.cpp
@@ -12,7 +12,7 @@ Player::Player() {
     x = 1, y = 1;
     mp = 100, hp = 100, energy = 100, maxEnergy = 100;
     maxMp = 100, maxHp = 100, exp = 0, maxExp = 10, damage = 2, defense = 0, weaponEnergy = 1, weaponMp = 0;
-    level = 1, gameLevel = 1;
+    level = 1, gameLevel = 4;
     race = "human";
     weapon = "", armor = "";
     debuffs.push_back(allDebuffs[0]);
