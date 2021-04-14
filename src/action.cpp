3bdbdc5f36327adc8ca6830ae7cc71bb638d9553
Ada <adaau0209@gmail.com>
2021-04-15 00:52:08 +0800

    updated a few words

--- src/action.cpp
+++ src/action.cpp
@@ -1,9 +1,9 @@
 #include "action.h"
 #include "backpack.h"
-#include <iostream>
-#include <thread>
 #include <iomanip>
+#include <iostream>
 #include <stdlib.h>
+#include <thread>
 #if defined _WIN32 || defined _WIN64
 #include <conio.h>
 #define ISWINDOW true
@@ -18,7 +18,7 @@ void action(Screen scr, Map map, Player player, Item item[], bool &isReplay) {
     char key = ' ';
     while (key != 'q') {
         scr.renderScreen(map, player);
-        bool wall = false;
+        bool wall = false, isValid = true;
         key = getch();
         switch (key) {
         case 'w':
@@ -61,55 +61,58 @@ void action(Screen scr, Map map, Player player, Item item[], bool &isReplay) {
         default:
             if (player.hp > 0)
                 scr.log = "Please input again or press [H] for help";
+            isValid = false;
         }
 
-        if (map.layout[player.y][player.x] == 'S') {
-            player.gameLevel++;
-            bool isEnd = true;
-            if (player.gameLevel % 5 == 0) {
-                cout << player.gameLevel / 5;
-                // bossScreen(player, item, player.gameLevel / 5, isEnd);
+        if (isValid) {
+            if (map.layout[player.y][player.x] == 'S') {
                 player.gameLevel++;
-                if (isEnd) {
-                    ending();
-                    cout << "You win the game! Wanna play again??\ny - Yes, what a fun game!\tn - No, I have better things to do with my life." << endl;
-                    tryAgain("Please input again:\ny - Yes, what a fun game!\tn - No, I have better things to do with my life.", isReplay);
-                    break;
+                bool isEnd = true;
+                if (player.gameLevel % 5 == 0) {
+                    cout << player.gameLevel / 5;
+                    // bossScreen(player, item, player.gameLevel / 5, isEnd);
+                    player.gameLevel++;
+                    if (isEnd) {
+                        ending();
+                        cout << "\nYou win the game! Wanna play again??\ny - Yes, what a fun game!\tn - No, I have better things to do with my life." << endl;
+                        tryAgain("Please input again:\ny - Yes, what a fun game!\tn - No, I have better things to do with my life.", isReplay);
+                        break;
+                    }
                 }
+                map.fill();
+                player.x = 1;
+                player.y = 1;
+            } else if (map.layout[player.y][player.x] == 'M') {
+                scr.log = "Monster!";
+                scr.renderScreen(map, player);
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                bool isEscape = false;
+                fightScreen(player, item, isEscape);
+                if (!isEscape) {
+                    map.removeMonster(player.x, player.y);
+                }
+                key = getch();
+                cin.ignore();
+            } else {
+                map.update();
             }
-            map.fill();
-            player.x = 1;
-            player.y = 1;
-        } else if (map.layout[player.y][player.x] == 'M') {
-            scr.log = "Monster!";
-            scr.renderScreen(map, player);
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-            bool isEscape = false;
-            fightScreen(player, item, isEscape);
-            if (!isEscape) {
-                map.removeMonster(player.x, player.y);
+
+            if (player.hp <= 0) {
+                cout << "You die:( Wanna try again?\ny - Yes, I want to try again!\tn - No, I suck at this game." << endl;
+                tryAgain("Please input again:\ny - Yes, I want to try again!\tn - No, I suck at this game.", isReplay);
+                break;
             }
-            key = getch();
-            cin.ignore();
-        } else {
-            map.update();
-        }
 
-        if (player.hp <= 0) {
-            cout << "You die:( Wanna try again?\ny - Yes, I want to try again!\tn - No, I suck at this game." << endl;
-            tryAgain("Please input again:\ny - Yes, I want to try again!\tn - No, I suck at this game.", isReplay);
-            break;
-        }
-
-        if (wall)
-            scr.log = "There is a wall in my way";
-        else {
-            if (player.energy > 0)
-                player.energy--;
-            else
-                player.energy = 0;
-            updateOnBuff(player);
-        clearScreen();
+            if (wall)
+                scr.log = "There is a wall in my way";
+            else {
+                if (player.energy > 0)
+                    player.energy--;
+                else
+                    player.energy = 0;
+                updateOnBuff(player);
+            }
+            clearScreen();
         }
     }
 }
@@ -150,10 +153,10 @@ void printBoatsInEnding() {
     }
 }
 
-void tryAgainInEnding(string &choice, bool &choosing){       //To-do: adding condition -> player's property
+void tryAgainInEnding(string &choice, bool &choosing) {    //To-do: adding condition -> player's property
     printBoatsInEnding();
     getline(cin, choice);
-    while (choosing){
+    while (choosing) {
         if (choice[1] == '\0' && choice[0] == '1') {
             renderNpc("charon", "You're broke before you came?");
             std::this_thread::sleep_for(std::chrono::milliseconds(500));
@@ -168,7 +171,7 @@ void tryAgainInEnding(string &choice, bool &choosing){       //To-do: adding con
             cout << "Bad Ending: Working in Hell.\n";
             choosing = false;
             break;
-        } else if (choice[1] == '\0' && (choice[0] == '2' || choice[0] == '3') ) {
+        } else if (choice[1] == '\0' && (choice[0] == '2' || choice[0] == '3')) {
             renderNpc("charon", "Can't you see it is SOLD OUT??");
             cout << "Enter again: ";
             tryAgainInEnding(choice, choosing);
@@ -203,22 +206,24 @@ void tryAgainInEnding(string &choice, bool &choosing){       //To-do: adding con
             renderNpc("charon", "No such choice.");
             tryAgainInEnding(choice, choosing);
             break;
-        }        
+        }
     }
 }
 
 void ending() {
     string choice = " ";
     bool choosing = true;
-    cout << "At the end, you defeated Hades, the Lord of Hell, through out your endless, extraordinary journey here.\n";
-    cout << "You feel drained out after such long battles, playing around the cold and dark, danger and monsters\n";
+    cout << "At the end, you defeated Hades, the Lord of Hell, in your endless, extraordinary journey of hell.\n";
+    cout << "You feel drained out after such long battles, playing around the cold and dark, gods and monsters\n";
     cout << "Closing your eyes, you were just desperate for a moment of rest...\n";
-    cout << "A whistle passes into your ears, a familiar voice...";
+    cout << "A whistle passes into your ears, a familiar voice...(press enter to continue)";
     cin.get();
     cout << "\n\"Oh, you come back to buy boats?\"\n";
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    renderNpc("charon", "Many people come and buy mt boats.");
-    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    std::this_thread::sleep_for(std::chrono::milliseconds(800));
+    renderNpc("charon", "Many people came and buy boats.");
+    std::this_thread::sleep_for(std::chrono::milliseconds(800));
+    renderNpc("charon", "Not many was left.");
+    std::this_thread::sleep_for(std::chrono::milliseconds(800));
     renderNpc("charon", "Which one do you want?");
     tryAgainInEnding(choice, choosing);
 }
